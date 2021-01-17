09de8a4e907d0c5befb3ba12c1e4a4aba1d8ba0e
MoatazDj <djebalimootez1@gmail.com>
2021-01-17 17:54:15 +0100

    initializing the courses actions

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import {Route, IndexRoute} from "react-router";
 import App from './Components/App'
 import HomePage from './Components/Home/Homepage'
 import AboutPage from './Components/About/Aboutpage'
+import CoursesPage from "./Components/Courses/CoursesPage"
 
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={HomePage}/>
+        <Route path="courses" component={CoursesPage}/>
         <Route path="about" component={AboutPage}/>
     </Route>    
 )
